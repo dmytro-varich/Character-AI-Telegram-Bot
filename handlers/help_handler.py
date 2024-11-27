c6b55fd6eff0948e4f0797a5d1e3029091dc2470
Dmytro Varich <136006220+dmytro-varich@users.noreply.github.com>
2024-11-27 13:04:32 +0100

    Update help_handler.py

--- handlers/help_handler.py
+++ handlers/help_handler.py
@@ -12,6 +12,5 @@ help_router: Router = Router()
 
 @help_router.message(ChatTypeFilter(chat_type=["private"]), Command('info'))
 async def cmd_help(message: Message) -> None:
-    if message.from_user.id == OWN_ID or message.from_user.id == OWN_ID_2:
-        await add_data(OWN_ID_2, '5158003cfcb2b9dcf16d04eb2f95c53b262d57a3', 'MtZ9L2BaB7MVT4q6vV4_jrR9DWScuDUHM_0EE8BOAlM', '9a77ae26-e10f-4b20-a38c-1ee6a8cd7ebc')
+    if message.from_user.id == OWN_ID or message.from_user.id == OWN_ID_2:
         await message.reply(info_message)
